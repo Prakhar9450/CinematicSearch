f2384b82a653592898518f2fc58f7c760ec778c0
Prakhar Pathak <prakharpathak9450@gmail.com>
2024-07-15 11:26:05 +0530

    Update pages

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import Contextpage from "../Contextpage";
+import { Helmet } from "react-helmet";
 
 function Genre() {
   const { fetchGenre, activegenre, setActiveGenre, genres } =
@@ -21,20 +22,26 @@ function Genre() {
   // }
 
   return (
-    <div className="flex flex-wrap justify-center px-2">
-      {genres.map((genre) => (
-        <button
-          onClick={() => setActiveGenre(genre.id)}
-          className={
-            activegenre === genre.id
-              ? "active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl"
-              : "px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl"
-          }
-          key={genre.id}>
-          {genre.name}
-        </button>
-      ))}
-    </div>
+    <>
+      <Helmet>
+        <title>CinematicSearch | Genres</title>
+      </Helmet>
+
+      <div className="flex flex-wrap justify-center px-2">
+        {genres.map((genre) => (
+          <button
+            onClick={() => setActiveGenre(genre.id)}
+            className={
+              activegenre === genre.id
+                ? "active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl"
+                : "px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl"
+            }
+            key={genre.id}>
+            {genre.name}
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { Helmet } from "react-helmet";
 import Contextpage from "../Contextpage";
 
 function Searchbar() {
@@ -22,20 +23,26 @@ function Searchbar() {
   };
 
   return (
-    <div className="w-full bg-gradient-to-r from-fuchsia-500 to-cyan-500 h-[10rem] md:h-[12rem]">
-      <div className="h-full w-full bg-black/30 flex justify-center items-center">
-        <input
-          type="search"
-          name="searchpanel"
-          id="searchpanel"
-          placeholder="Search movie"
-          className="p-3 w-full mx-10 md:w-[40rem]  rounded-xl outline-none"
-          onKeyDown={(e) => onKeyUp(e)}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+    <>
+      <Helmet>
+        <title>CinematicSearch</title>
+      </Helmet>
+
+      <div className="w-full bg-gradient-to-r from-fuchsia-500 to-cyan-500 h-[10rem] md:h-[12rem]">
+        <div className="h-full w-full bg-black/30 flex justify-center items-center">
+          <input
+            type="search"
+            name="searchpanel"
+            id="searchpanel"
+            placeholder="Search movie"
+            className="p-3 w-full mx-10 md:w-[40rem]  rounded-xl outline-none"
+            onKeyDown={(e) => onKeyUp(e)}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
