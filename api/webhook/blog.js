635938437bd3ae3d7e23c85f8ed0da0c46dde235
Prakhar Pathak <prakharpathak9450@gmail.com>
2026-03-15 15:46:21 +0530

    Remove contentImageUrl from blog post handler to streamline data structure

--- api/webhook/blog.js
+++ api/webhook/blog.js
@@ -36,7 +36,6 @@ export default async function handler(req, res) {
         contentMarkdown: data.content_markdown || "",
         metaDescription: data.meta_description || null,
         featuredImageUrl: data.featured_image_url || null,
-        contentImageUrl: data.content_image_url || null,
         tags: data.tags || [],
         publishedAt: data.created_at
           ? new Date(data.created_at)
